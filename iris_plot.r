bf18d313f1fe2d7ec50032e53cd4c12ccb3a3874
willrowe12345 <willrowe12345@gmail.com>
2025-11-21 22:10:43 +0000

    old version of title

--- iris_plot.r
+++ iris_plot.r
@@ -7,7 +7,7 @@ ggplot(iris, aes(x= Petal.Length, y= Petal.Width, fill=Species)) +
   geom_point(alpha = 0.8) +
   labs(x = "Petal length (cm)",
        y= "Petal Width (cm)",
-       title= "Petal width against length by species 2",
+       title= "Petal width against length by species",
        subtitle = "Points show data lines show the trends of the flowers") +
   theme_bw() +
   geom_smooth(aes(colour= Species))
