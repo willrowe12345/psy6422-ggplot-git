141333b00ea91c7ee5805dd78991fea454d1ff89
willrowe12345 <willrowe12345@gmail.com>
2025-11-21 20:19:43 +0000

    Done task a - made a scatterplot for petal lenght vs width using the iris data with custimosed trends

---
+++ iris_plot.r
@@ -0,0 +1,13 @@
+library(ggplot2)
+library(dplyr)
+
+data(iris)
+
+ggplot(iris, aes(x= Petal.Length, y= Petal.Width, fill=Species)) +
+  geom_point(alpha = 0.7) +
+  labs(x = "Petal length (cm)",
+       y= "Petal Width (cm)",
+       title= "Petal width against length by species",
+       subtitle = "Points show data lines show the trends of the flowers") +
+  theme_minimal() +
+  geom_smooth(aes(colour= Species))
